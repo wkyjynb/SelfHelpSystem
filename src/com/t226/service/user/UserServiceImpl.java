9efd78a9a36dac7707a094cfa3e6fc8c47cb7016
zhang309 <859476472@qq.com>
2019-05-28 16:52:21 +0800

    雷湘威777777777777777777777777

--- src/com/t226/service/user/UserServiceImpl.java
+++ src/com/t226/service/user/UserServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.annotation.Resource;
 
 import com.mysql.jdbc.StringUtils;
+import com.t226.pojo.Detection;
 import org.springframework.stereotype.Service;
 
 import com.t226.dao.user.UserMapper;
@@ -37,6 +38,29 @@ public class UserServiceImpl implements UserService {
 			}
 		}
 		return null;
+	}
+	@Override
+	public User dologin(String userCode,String userPassword) {
+		User user=userMapper.dologin(userCode);
+		System.out.println(user!=null);
+		if(user!=null){
+			if(user.getPassword().equals(userPassword)){
+				return user;
+			}
+		}
+		return null;
+	}
+	@Override
+	public boolean doInsert(Detection detection) {
+		return userMapper.doInsert(detection);
+	}
+	@Override
+	public List<Detection> finSelect() {
+		return userMapper.finSelect();
+	}
+	@Override
+	public List<Object> finCount(String date, String date1) {
+		return userMapper.finCount(date, date1);
 	}
 
 	//进行身份认证
